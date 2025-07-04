9183d6b9fac2bdadd16824145115dacfe041fe58
Andy Maskiell <maskiella@gmail.com>
2025-07-04 15:10:02 -0400

    fix: fix layouts components

--- src/layouts/FlexRow/index.tsx
+++ src/layouts/FlexRow/index.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
+import classNames from 'classnames';
 
 import './styles.css';
 
 export type FlexRowProps = React.PropsWithChildren<React.HTMLProps<HTMLDivElement>>;
 
-export function FlexRow({ children, ...props }: FlexRowProps) {
-    return (
-        <div className="flex-row" {...props}>
-            {children}
-        </div>
-    );
+export function FlexRow({ children, className, ...props }: FlexRowProps) {
+  return (
+    <div className={classNames('flex-row', className)} {...props}>
+      {children}
+    </div>
+  );
 }
